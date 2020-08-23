c6ce456ba9fb69f8f9b26dd1bd6363c0d306449c
msl <masloff@zoho.com>
2020-08-23 19:43:45 +0500

    Update 23.08.2002

---
+++ core/class/MNotification.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * The class manages the notifications sent by the system.
+ */
+
+class MNotification
+{
+
+    private $data = [];
+
+    public function send ($id=0, $priority=1, $title='New notify', $content='') {
+
+        foreach ($notification as $data) {
+            die($notification);
+        }
+
+    }
+
+}
