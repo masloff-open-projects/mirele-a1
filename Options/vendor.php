5a9a3b3f4554a5040440f9ed3dfcdb96858b682f
msl <masloff@zoho.com>
2020-09-21 18:49:40 +0500

    Update 21.09.20

--- Options/vendor.php
+++ Options/vendor.php
@@ -1,5 +1,28 @@
 <?php
 
+/**
+ *
+ * ██╗   ██╗███████╗███╗   ██╗██████╗  ██████╗ ██████╗
+ * ██║   ██║██╔════╝████╗  ██║██╔══██╗██╔═══██╗██╔══██╗
+ * ██║   ██║█████╗  ██╔██╗ ██║██║  ██║██║   ██║██████╔╝
+ * ╚██╗ ██╔╝██╔══╝  ██║╚██╗██║██║  ██║██║   ██║██╔══██╗
+ *  ╚████╔╝ ███████╗██║ ╚████║██████╔╝╚██████╔╝██║  ██║
+ *   ╚═══╝  ╚══════╝╚═╝  ╚═══╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝
+ *
+ * Vendor files are used to subclick a whole set
+ * of target bit files included in the complex.
+ * Vendor files always lie in the root directory
+ * of the complex target bit divisions into separate files.
+ * Vendor files always specify the target file and cannot
+ * connect foreign files to the system, they cannot subdivide through the loop
+ *
+ * @vendor Options
+ * @version 1.0.0
+ * @author Mirele
+ * @alias vendor-options
+ * @template vendor
+ */
+
 require_once 'basic/mrl_wp_navbar_fixed.php';
 require_once 'basic/mrl_wp_sidebar_width_1_active.php';
 require_once 'basic/mrl_wp_sidebar_width_2_active.php';
--- Routes/vendor.php
+++ Routes/vendor.php
@@ -1,5 +1,28 @@
 <?php
 
+/**
+ *
+ * ██╗   ██╗███████╗███╗   ██╗██████╗  ██████╗ ██████╗
+ * ██║   ██║██╔════╝████╗  ██║██╔══██╗██╔═══██╗██╔══██╗
+ * ██║   ██║█████╗  ██╔██╗ ██║██║  ██║██║   ██║██████╔╝
+ * ╚██╗ ██╔╝██╔══╝  ██║╚██╗██║██║  ██║██║   ██║██╔══██╗
+ *  ╚████╔╝ ███████╗██║ ╚████║██████╔╝╚██████╔╝██║  ██║
+ *   ╚═══╝  ╚══════╝╚═╝  ╚═══╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝
+ *
+ * Vendor files are used to subclick a whole set
+ * of target bit files included in the complex.
+ * Vendor files always lie in the root directory
+ * of the complex target bit divisions into separate files.
+ * Vendor files always specify the target file and cannot
+ * connect foreign files to the system, they cannot subdivide through the loop
+ *
+ * @vendor Routes
+ * @version 1.0.0
+ * @author Mirele
+ * @alias vendor-routes
+ * @template vendor
+ */
+
 // AJAX
 require_once 'Complex/wp-admin/Compound-createPage.php';
 require_once 'AJAX/login.php';
